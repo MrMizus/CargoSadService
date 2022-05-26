bc980781a00a5a2a08c8f1ac64ceada363e6d431
Michał <jankowski.michal964@gmail.com>
2022-05-26 12:26:43 +0200

    change structure

--- src/js/main.js
+++ src/js/main.js
@@ -1,34 +1,35 @@
-const burgerBtn = document.querySelector('.burger-btn');
-const burgerMenu = document.querySelector('.burger-menu');
-const burgerItems = document.querySelectorAll('.burger-menu__link');
+const burgerBtn = document.querySelector('.nav__burger-btn');
+const burgerMenu = document.querySelector('.nav__burger-menu');
+const burgerItems = document.querySelectorAll('.nav__burger-menu-link');
 const nav = document.querySelector('.nav');
-const logo = document.querySelector('.logo');
+const logo = document.querySelector('.nav__logo');
 const loader = document.querySelector('.loader-wrapper')
 
 const showBurgerMenu = () => {
-	burgerMenu.classList.toggle('burger-menu__show');
-	burgerMenu.classList.toggle('burger-menu__hide');
-	burgerMenu.classList.remove('burger-menu__opacity');
+	burgerMenu.classList.toggle('nav__burger-menu-show');
+	burgerMenu.classList.toggle('nav__burger-menu-hide');
+	burgerMenu.classList.remove('nav__burger-menu-opacity');
 
 	burgerItems.forEach((item) => {
 		item.addEventListener('click', () => {
-			burgerMenu.classList.add('burger-menu__hide');
-			burgerMenu.classList.remove('burger-menu__show');
+			burgerMenu.classList.add('burger-menu-hide');
+			burgerMenu.classList.remove('burger-menu-show');
 		});
 	});
 
 	logo.addEventListener('click', () => {
-		burgerMenu.classList.add('burger-menu__hide');
-		burgerMenu.classList.remove('burger-menu__show');
+		burgerMenu.classList.add('burger-menu-hide');
+		burgerMenu.classList.remove('burger-menu-show');
 	});
 };
 burgerBtn.addEventListener('click', showBurgerMenu);
 document.addEventListener('DOMContentLoaded', function () {
 	const addShadow = () => {
 		if (window.scrollY >= 200) {
-			nav.classList.add('shadow-nav');
+			nav.classList.add('nav__shadow');
+			console.log("test");
 		} else {
-			nav.classList.remove('shadow-nav');
+			nav.classList.remove('nav__shadow');
 		}
 	};
 
